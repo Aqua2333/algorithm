a2573320fff4b492f54a019f5b9cfcdab728c776
e <1754077099@qq.com>
2019-09-17 10:13:11 +0800

    插入排序

--- src/main/java/com/algorithm/InsertionSort.java
+++ src/main/java/com/algorithm/InsertionSort.java
@@ -34,8 +34,8 @@ public class InsertionSort {
             while (j >= 0 && array[j] < key) {
                 array[j + 1] = array[j];
                 j = j - 1;
-                array[j + 1] = key;
             }
+            array[j + 1] = key;
         }
         logger.info("排序完成...");
         return array;
